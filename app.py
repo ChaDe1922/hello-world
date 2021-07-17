2790e6eb343cc369e0c29879979f1fe19723b539
Chalece Delacoudray <cdelacoudray@codio.com>
2021-07-17 19:48:02 +0000

    Style sweeper 1

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Hello there world!"
+    return "Hello there world!"
 
+  
 if  __name__ == "__main__":
   app.run()
+  
